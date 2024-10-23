410095d2621c988472805e8f4c31d7f396eb03c7
Jeff Knaggs <mail@jeef3.com>
2024-10-24 09:22:15 +1300

    Add basic form elements

--- src/modals/AddEditEventModal.tsx
+++ src/modals/AddEditEventModal.tsx
@@ -1,20 +1,88 @@
-import { MouseEventHandler } from "react";
+import { FormEvent, MouseEventHandler, useCallback } from "react";
 
 import Modal from "../components/Modal";
 import ModalHeader from "../components/ModalHeader";
 import ModalFooter from "../components/ModalFooter";
+import useForm from "../hooks/useForm";
+import { CalendarEvent } from "../types";
 
 export default function AddEditEventModal({
   onClose,
 }: {
   onClose?: MouseEventHandler;
 }) {
+  const { formData, formState, handleChange, handleSubmit, setError } =
+    useForm<CalendarEvent>();
+
+  const onSubmit = useCallback(
+    (e: FormEvent) => void handleSubmit(async (event: CalendarEvent) => {})(e),
+    [handleSubmit],
+  );
+
   return (
     <Modal>
-      <form>
+      <form onSubmit={onSubmit}>
         <ModalHeader title="Add Event" onClose={onClose} />
 
-        <div>FORM</div>
+        <div>
+          <label>
+            Name
+            <input
+              required
+              type="text"
+              name="name"
+              disabled={formState.isSubmitting}
+              value={formData.title}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Description
+            <textarea
+              name="description"
+              disabled={formState.isSubmitting}
+              value={formData.description}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Starts
+            <input
+              required
+              type="date"
+              name="name"
+              disabled={formState.isSubmitting}
+              value={formData.start}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Ends
+            <input
+              required
+              type="date"
+              name="name"
+              disabled={formState.isSubmitting}
+              value={formData.end}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            All day?
+            <input
+              required
+              type="checkbox"
+              name="name"
+              disabled={formState.isSubmitting}
+              checked={formData.allDay}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
 
         <ModalFooter />
       </form>

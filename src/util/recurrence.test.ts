8dfc972738f94216d1b19a304f90136859354d55
Jeff Knaggs <mail@jeef3.com>
2024-10-25 17:05:28 +1300

    Add some recurrence generation logic

---
+++ src/util/recurrence.test.ts
@@ -0,0 +1,58 @@
+import { describe, expect, test } from "vitest";
+import { generateRecurring } from "./recurrence";
+
+describe("Recurrence", () => {
+  describe("generateRecurring", () => {
+    test("It generates 10 recurrences", () => {
+      const eventDate = new Date();
+
+      const result = generateRecurring(eventDate, "weekly");
+
+      expect(result.length).toBe(10);
+    });
+
+    test("Weekly recurrences are on the same day each week", () => {
+      const eventDate = new Date("2024-01-01");
+
+      const result = generateRecurring(eventDate, "weekly");
+
+      expect(result[0].toISOString().slice(0, 10)).toBe("2024-01-01");
+      expect(result[1].toISOString().slice(0, 10)).toBe("2024-01-08");
+      expect(result[2].toISOString().slice(0, 10)).toBe("2024-01-15");
+      expect(result[3].toISOString().slice(0, 10)).toBe("2024-01-22");
+    });
+
+    test("Monthly recurrences are on the same date each month", () => {
+      const eventDate = new Date("2024-01-10");
+
+      const result = generateRecurring(eventDate, "monthly");
+
+      expect(result[0].toISOString().slice(0, 10)).toBe("2024-01-10");
+      expect(result[1].toISOString().slice(0, 10)).toBe("2024-02-10");
+      expect(result[2].toISOString().slice(0, 10)).toBe("2024-03-10");
+      expect(result[3].toISOString().slice(0, 10)).toBe("2024-04-10");
+    });
+
+    test("Monthly recurrence dates that don't exist, recure on the last of the month", () => {
+      const eventDate = new Date("2024-01-31");
+
+      const result = generateRecurring(eventDate, "monthly");
+
+      expect(result[0].toISOString().slice(0, 10)).toBe("2024-01-31");
+      expect(result[1].toISOString().slice(0, 10)).toBe("2024-02-29");
+      expect(result[2].toISOString().slice(0, 10)).toBe("2024-03-31");
+      expect(result[3].toISOString().slice(0, 10)).toBe("2024-04-30");
+    });
+
+    test("Yearly recurrences are on the same date each year", () => {
+      const eventDate = new Date("2024-01-10");
+
+      const result = generateRecurring(eventDate, "yearly");
+
+      expect(result[0].toISOString().slice(0, 10)).toBe("2024-01-10");
+      expect(result[1].toISOString().slice(0, 10)).toBe("2025-01-10");
+      expect(result[2].toISOString().slice(0, 10)).toBe("2026-01-10");
+      expect(result[3].toISOString().slice(0, 10)).toBe("2027-01-10");
+    });
+  });
+});
---
+++ src/util/recurrence.ts
@@ -0,0 +1,54 @@
+import { RecurrenceFrequency } from "../types";
+
+const COUNT = 10;
+
+export function generateRecurring(date: Date, recurrence: RecurrenceFrequency) {
+  if (recurrence === "weekly") {
+    return Array.from(new Array(COUNT - 1)).reduce(
+      (p) => {
+        const nextDate = new Date(p[p.length - 1]);
+        nextDate.setDate(nextDate.getDate() + 7);
+
+        return [...p, nextDate];
+      },
+      [date],
+    );
+  }
+
+  if (recurrence === "monthly") {
+    return Array.from(new Array(COUNT - 1)).reduce(
+      (p, _d, i) => {
+        const current = p[p.length - 1];
+
+        const nextDate = new Date(current);
+        nextDate.setMonth(nextDate.getMonth() + 1, date.getDate());
+
+        // JavaScript will adjust the month for invalid dates if they go over.
+        // We need to clamp to the last day of the month.
+        if (nextDate.getMonth() !== current.getMonth() + 1) {
+          const clampedDate = new Date(date);
+          clampedDate.setMonth(clampedDate.getMonth() + 2 + i, 0);
+
+          return [...p, clampedDate];
+        }
+
+        return [...p, nextDate];
+      },
+      [date],
+    );
+  }
+
+  if (recurrence === "yearly") {
+    return Array.from(new Array(COUNT - 1)).reduce(
+      (p) => {
+        const nextDate = new Date(p[p.length - 1]);
+        nextDate.setFullYear(nextDate.getFullYear() + 1);
+
+        return [...p, nextDate];
+      },
+      [date],
+    );
+  }
+
+  return [];
+}
